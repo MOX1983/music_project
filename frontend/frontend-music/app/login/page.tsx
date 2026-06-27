8a8fa0ec3cfbba65b5e0de76030c3509988ec789
Mox <tenmaken246@gmail.com>
2026-06-28 01:21:17 +0300

    update

--- frontend/frontend-music/app/login/page.tsx
+++ frontend/frontend-music/app/login/page.tsx
@@ -1,7 +1,19 @@
 import styles from "../../styles/styles.module.css"
 import Link from "next/link"
+import { useEffect, useState } from "react";
 
 export default function Login() {
+  const [users, setUsers] = useState([]);
+
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/login")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error("Error fetching users:", error));
+  }, []);
+
+
   return <div className={styles.body}>
     <div className="main" >
               <h1>LOGIN</h1>
--- frontend/frontend-music/app/page.tsx
+++ frontend/frontend-music/app/page.tsx
@@ -1,14 +1,57 @@
+"use client";
+
 import styles from "./styles.module.css";
 import plug from "../public/img/cat.jpg";
 import Track from "../components/Track";
 import Playlist from "../components/Playlist";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { title } from "process";
+
+const API_URL = 'http://127.0.0.1:8000';
+
+interface User {
+  user_id: number;
+  login: string;
+  email: string;
+  photo?: string;
+}
+
+interface Track {
+  track_id: number;
+  title: string;
+  author: string;
+  path_file: string;
+  duration: string;
+  category?: string;
+  picture?: string;
+}
+
 
 export default function Main() {
+  const [user, setUsers] = useState<User | null>(null);
+  const [tracks, setTracks] = useState<Track[]>([]);
+  const [track, setTrack] = useState<Track | null>(null);
+
+
+  useEffect(() => {
+    fetch(`${API_URL}/me`)
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error("Error fetching users:", error));
+  }, []);
+
+  useEffect(() => {
+    fetch(`${API_URL}/tracks`)
+      .then((response) => response.json())
+      .then((data) => setTracks(data))
+      .catch((error) => console.error("Error fetching tracks:", error));
+  }, []);
+
   return (
     <div className={styles.body}>
       <header>
-        <Image className="logo" src={plug.src} alt={""} width={50} height={50}></Image>
+        <Image className="logo" src={user?.photo || plug.src} alt={""} width={50} height={50}></Image>
         <input type="text" placeholder="search" />
       </header>
       <div className={styles.mainBody}>
@@ -17,16 +60,17 @@ export default function Main() {
           <Playlist name={"name playlist"}></Playlist>
         </div>
         <div className="main-panel">
-          <Track title="test" img={plug.src}></Track>
 
-          <Track title="test" img={plug.src}></Track>
-          <Track title="test" img={plug.src}></Track>
+          {tracks.map((track) =>(
+            <Track title={track?.title} img={track.picture || plug.src}></Track>
+          ))}
+          
         </div>
       </div>
 
       <div className="player">
         <Image className="img-m" src={plug.src} alt={""} width={20} height={20}></Image>
-        <audio controls src="/mp3/Husky_Rescue-Sound_of_Love.mp3"></audio>
+        <audio controls src=""></audio>
       </div>
     </div>
   );
