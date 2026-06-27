c2e7b599095cf423386f817e424d6054350e1cb8
Mox <tenmaken246@gmail.com>
2026-06-27 23:24:17 +0300

    add photo in users

--- backend/app/schemes/user.py
+++ backend/app/schemes/user.py
@@ -5,12 +5,15 @@ class UserBase(BaseModel):
     password_hash: str = Field(min_length=8, max_length=255, alias="password_hash")
     email: EmailStr
 
-    # валидация на login
-
     model_config = ConfigDict(from_attributes=True)
 
 
 class UserResponse(UserBase):
     user_id: int
 
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserAll(UserResponse):
+    photo: str
     model_config = ConfigDict(from_attributes=True)
